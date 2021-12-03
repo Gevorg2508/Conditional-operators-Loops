034e6c7db22f8e2a09268e9cf6dde35265967f7b
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-03 12:30:12 +0400

    i change the exercise 11

--- 11.js
+++ 11.js
@@ -1,12 +1,50 @@
-// function calc(num1,num2){
-//      if(num2 === 0){
-//         console.log("cannot calculate !");
-//      }else if(num2 % num1 ===0){
-//         console.log("quitient is "+(num2/num1)+"!");
-//     }
-//     else if(num2 % num1 !==0){
-//         console.log("remainder is "+(num2 % num1))
-//     }
-//     return ;
-// }
-// calc(5,10);
+ let product = 1;
+ let sum = 0;
+function func1(num){
+   
+let str1 = num.toString();
+let arr1 = str1.split('');
+if(num === 0){
+    return 0;
+}else {
+for(let i = 0; i<arr1.length; i++){
+    
+   product *= arr1[i];
+}
+}
+return product ; 
+}
+
+
+function func2(num){
+    let str1 = num.toString();
+    let arr1 = str1.split('');
+    if(num === 0 ){
+        return 0;
+    }else {
+    for(let i = 0; i<arr1.length; i++){
+        
+       sum += Number(arr1[i]);
+    }
+}
+return sum;
+}
+
+
+function result(product, sum){
+if (product % sum ===0){
+    console.log('Quotient is '+product/sum);
+}else if(product === 0 || sum ===0){
+    console.log("cannot calculate");
+}else if(product % sum !==0){
+console.log("Remainder is "+(product % sum))
+}else if(sum === 1)
+{
+console.log(product);
+}
+
+return ;
+}
+func1(0);
+func2(0);
+result(product, sum);
