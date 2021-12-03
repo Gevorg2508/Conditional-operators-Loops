01e06d8af812465bb2f3a4667f8c6cad7d8265eb
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-03 14:09:06 +0400

    ichanged the exersice 12!

--- 12.js
+++ 12.js
@@ -1,10 +1,18 @@
-function replace(){
-let arr = ["Houston", "problem"];
-let sent = "_, we have a _.";
-let sent1 = sent.split('');
-for (let i = 0;i < sent1.length;i ++){
-    if(sent1[i] == "_"){
-   sent1.splice(i,1,arr[i--]);
+let str = '_, we have a _.' ;
+let arr = ['Houston', 'problem'];
+let index = 0;
+let arr2 = [];
+
+function esc(str){
+    let arr1 = str.split('');
+for (let i = 0; i <arr1.length; i++){
+        if(arr1[i] === '_'){
+            arr2.push(arr[index]);
+            index ++;
+        }else {
+            arr2.push(arr1[i]);
+        }
     }
+return arr2.join('');
 }
-}
+console.log(esc(str));
